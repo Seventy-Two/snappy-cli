17f1b2d95b106c8738664e39591e9b264f5bc303
Megz <seventy-two@live.com>
2018-05-18 11:43:45 +0100

    Work on directories and files
    Operation closer to bunzip2 rather than taking input from stdin and outputting from stdout

--- cmd/snappy-uncompress/main.go
+++ cmd/snappy-uncompress/main.go
@@ -2,24 +2,51 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/golang/snappy"
 )
 
 func main() {
-	r := os.Stdin
-	if len(os.Args) >= 2 {
-		srcPath := os.Args[1]
-		fr, err := os.Open(srcPath)
+	srcPath := os.Args[1]
+	files, err := ioutil.ReadDir(srcPath)
+	if err != nil {
+		err = openAndUncompress(srcPath)
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer fr.Close()
-		r = fr
+	}
+	for _, file := range files {
+		if !file.IsDir() {
+			err := openAndUncompress(srcPath + string(os.PathSeparator) + file.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 
-	sr := snappy.NewReader(r)
-	_, _ = io.Copy(os.Stdout, sr)
+}
+
+func openAndUncompress(filename string) error {
+	fr, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fr.Close()
+
+	outFile, err := os.Create(strings.Replace(filename, ".snappy", "", 1))
+	if err != nil {
+		return err
+	}
+	sr := snappy.NewReader(fr)
+
+	_, err = io.Copy(outFile, sr)
+	if err != nil {
+		return err
+	}
+	err = os.Remove(filename)
+	return err
 }
